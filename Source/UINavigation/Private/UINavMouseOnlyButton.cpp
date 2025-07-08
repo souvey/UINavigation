1f2ed03aa1446e23f268a1e028a81d625ccba3d2
Christopher Souvey <souvey@gmail.com>
2025-07-08 14:32:29 -0400

    Add a component for buttons that are only intended for the mouse

---
+++ Source/UINavigation/Private/UINavMouseOnlyButton.cpp
@@ -0,0 +1,46 @@
+﻿// 
+
+
+#include "UINavMouseOnlyButton.h"
+
+#include "UINavPCComponent.h"
+
+TSharedRef<SWidget> UUINavMouseOnlyButton::RebuildWidget()
+{
+	OriginalWidgetStyle = GetStyle();
+	if (APlayerController* PC = Cast<APlayerController>(GetOwningPlayer()))
+	{
+		if (UUINavPCComponent* UINavPC = PC->FindComponentByClass<UUINavPCComponent>())
+		{
+			UINavPC->InputTypeChangedDelegate.AddDynamic(this, &UUINavMouseOnlyButton::OnInputTypeChanged);
+		}
+	}
+	return Super::RebuildWidget();
+}
+
+void UUINavMouseOnlyButton::ReleaseSlateResources(bool bReleaseChildren)
+{
+	Super::ReleaseSlateResources(bReleaseChildren);
+	if (APlayerController* PC = Cast<APlayerController>(GetOwningPlayer()))
+	{
+		if (UUINavPCComponent* UINavPC = PC->FindComponentByClass<UUINavPCComponent>())
+		{
+			UINavPC->InputTypeChangedDelegate.RemoveDynamic(this, &UUINavMouseOnlyButton::OnInputTypeChanged);
+		}
+	}
+	OriginalWidgetStyle = {};
+}
+
+void UUINavMouseOnlyButton::OnInputTypeChanged(EInputType InputType)
+{
+	if (InputType == EInputType::Mouse)
+	{
+		SetStyle(OriginalWidgetStyle);
+	} else
+	{
+		FButtonStyle NoMouse = OriginalWidgetStyle;
+		NoMouse.Hovered = NoMouse.Normal;
+		NoMouse.Pressed = NoMouse.Normal;
+		SetStyle(NoMouse);
+	}
+}
---
+++ Source/UINavigation/Public/UINavMouseOnlyButton.h
@@ -0,0 +1,28 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "Data/InputType.h"
+#include "UINavMouseOnlyButton.generated.h"
+
+class UUINavPCComponent;
+/**
+ * 
+ */
+UCLASS()
+class UINAVIGATION_API UUINavMouseOnlyButton : public UButton
+{
+	GENERATED_BODY()
+
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+
+	UFUNCTION()
+	void OnInputTypeChanged(EInputType InputType);
+
+private:
+	UPROPERTY()
+	FButtonStyle OriginalWidgetStyle;	
+};
